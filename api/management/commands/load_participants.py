20d922f51c3d9290ac754949307e3631b37912df
hazarin <iovamckey@gmail.com>
2020-05-28 22:54:01 +0700

    Docker support fixes

--- api/management/commands/load_participants.py
+++ api/management/commands/load_participants.py
@@ -17,21 +17,17 @@ class Command(BaseCommand):
                 key = 0
                 for obj in reader:
                     key += 1
-                    participant = models.Participant(name=obj['name'])
-                    participant.save()
+                    participant = models.Participant.objects.create(name=obj['name'])
                     if key % 100 == 0:
                         self.stdout.write(self.style.SUCCESS(key))
                     for name, value in obj['precedents'].items():
                         try:
                             precedent_catalog = models.PrecedentCatalog.objects.get(name=name)
                         except models.PrecedentCatalog.DoesNotExist as e:
-                            precedent_catalog = models.PrecedentCatalog(name=name)
-                            precedent_catalog.save()
+                            precedent_catalog = models.PrecedentCatalog.objects.create(name=name)
                         attitude = 0 if value['attitude'] == 'negative' else 1
-                        precedent = models.Precedent(precedent=precedent_catalog, attitude=attitude,
+                        precedent = models.Precedent.objects.create(precedent=precedent_catalog, attitude=attitude,
                                                      importance=value['importance'], participant=participant)
-                        precedent.save()
-                    if key > 500: break
             self.stdout.write(self.style.SUCCESS('Data loaded'))
         except DatabaseError:
             raise CommandError('Database error')
